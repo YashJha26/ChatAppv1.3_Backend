5849411a8c8221607aa2f84180d3196626a3888a
YashJha26 <142669810+YashJha26@users.noreply.github.com>
2024-10-22 10:54:37 +0530

    Update authMiddleware.js,fixed bearer token

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,24 +4,26 @@ const authMiddleware = async (req,res,next) =>{
 
   // Check if the Authorization header is present and follows the Bearer scheme
     if (authHeader && authHeader.startsWith('Bearer ')) {
-    const token = authHeader.split(' ')[1]; // Extract the token part
-    console.log("Middleware token",token);
-    }
-    let verifiedToken;
-    try {
-        if(token){
-            verifiedToken= await jwt.verify(token,process.env.JWT_SECRET_KEY);
-            if(!verifiedToken){
-                res.json({message:"No token / token expired "}).redirect(process.env.CLIENT_AUTH_URL);
+        const token = authHeader.split(' ')[1]; // Extract the token part
+        console.log('middleware token:',token);
+        let verifiedToken;
+        try {
+            if(token){
+                verifiedToken= await jwt.verify(token,process.env.JWT_SECRET_KEY);
+                if(!verifiedToken){
+                    res.json({message:"No token / token expired "}).redirect(process.env.CLIENT_AUTH_URL);
+                }
+                req.user = verifiedToken;
+                next();
+            }else{
+                res.json({message:"No token / token expired "});
             }
-            req.user = verifiedToken;
-            next();
-        }else{
+        } catch (error) {
             res.json({message:"No token / token expired "});
+            next(error)
         }
-    } catch (error) {
-        res.json({message:"No token / token expired "});
-        next(error)
+    }else{
+        return res.status(401).json({ message: "Authorization header missing or malformed." });
     }
 }
 
